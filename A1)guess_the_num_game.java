4728cc46ab96fc68a126d2fdb8389bf4d4610e21
Bhavesh M. Dhake <bhavesh.dhake.ai@ghrcem.raisoni.net>
2025-01-07 12:23:49 +0530

    Create A1)guess_the_num_game.java

---
+++ A1)guess_the_num_game.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int mynum = (int)(Math.random()*100);
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Guess the number : (1-100) , enter '-1' to exit");
+            int usernum = sc.nextInt();
+
+            if(usernum == mynum){
+                System.out.println("Correct guess ");
+                break;
+            }
+            else if(usernum > mynum){
+                System.out.println("Wrong , guessed number too high ");
+            }
+            else if(usernum < mynum){
+                System.out.println("Wrong , guessed number too low");
+            }
+            else if(usernum == -1){
+                System.out.println("You lose .. the number is : " + mynum);
+                break;
+            }
+        }while(true);
+    }
+}
